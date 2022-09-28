edaf1daa6766778e506961ca7fda5cbc76e02fe0
PManiAwasthi <awasthiprakashmani7@gmail.com>
2022-09-28 13:29:28 +0530

    setup.py configured

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ def get_requirements_list() -> List[str]:
     """
     with open(REQUIREMENTS_FILE_NAME) as requirements_file:
         requirements_list = requirements_file.readlines()
-        requirements_list = [requirements_list.replace("\n", "") for requirement_name in requirements_list]
+        requirements_list = [requirement_name.replace("\n", "") for requirement_name in requirements_list]
         if HYPHEN_E_DOT in requirements_list:
-            requirement_name.remove(HYPHEN_E_DOT)
+            requirements_list.remove(HYPHEN_E_DOT)
         return requirements_list
 
 setup(
