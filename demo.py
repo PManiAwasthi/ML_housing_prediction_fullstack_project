a749de85cbda96591eafc3536a4f1fe0271c2f72
PManiAwasthi <awasthiprakashmani7@gmail.com>
2022-10-13 18:33:23 +0530

    data transformation initialised

--- demo.py
+++ demo.py
@@ -3,8 +3,10 @@ from housing.pipeline.pipeline import Pipeline
 from housing.logger import logging
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
+        # pipeline = Pipeline()
+        # pipeline.run_pipeline()
+        data_validation_config = Configuartion().get_data_validation_config()
+        print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
